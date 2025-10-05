a4acca2f29fb10908edd0f1bae52dad979136a6b
astowner <the1samevl@gmail.com>
2025-10-05 11:30:55 +0300

    feat: add hero section ex-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="bg-amber-500 min-h-screen max-w-full flex justify-center items-center">
+      <div className="bg-blue-300 w-[1024px] max-w-3xl p-4 h-[600px] flex justify-center items-center">
+        <div className="bg-red-300 h-[300px] flex-1">
+          <Image src={`/wolf.jpg`} alt="wolf" width={1024} height={1024} />
+        </div>
+        <div className="bg-green-300 h-[300px] flex-1">
+          <p>Hello world</p>
+        </div>
+      </div>
+    </div>
   );
 }
